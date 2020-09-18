0c7331cb88b8efc296ccd7c39040adadded2c7fa
ChrisLaiZimeng <1450583197@qq.com>
2020-09-18 21:12:51 +0800

    mobile adaptation

--- src/views/taskList/index.vue
+++ src/views/taskList/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="taskList" class="container-fluid">
     <div id="buttonBar" class="container-fluid">
-      <router-link to="/taskList/toDoList" class="tag" :class="{choosed: activeTag0}" @click.native="changeTag(0)">所有事项</router-link>
-      <router-link to="/taskList/notFinished" class="tag" :class="{choosed: activeTag1}" @click.native="changeTag(1)">代办事项</router-link>
-      <router-link to="/taskList/finishedTask" class="tag" :class="{choosed: activeTag2}" @click.native="changeTag(2)">已完成事项</router-link>
+      <router-link to="/taskList/toDoList" class="tag" :class="{choosed: activeTag0}" @click.native="changeTag(0)">所有任务</router-link>
+      <router-link to="/taskList/notFinished" class="tag" :class="{choosed: activeTag1}" @click.native="changeTag(1)">待完成</router-link>
+      <router-link to="/taskList/finishedTask" class="tag" :class="{choosed: activeTag2}" @click.native="changeTag(2)">已完成</router-link>
     </div>
     <router-view></router-view>
   </div>
@@ -55,6 +55,7 @@ export default {
     background-color: #7ed48b;
     padding: 1em;
     height: 100vh;
+    font-size: .8em;
   }
   #buttonBar{
     display: flex;
